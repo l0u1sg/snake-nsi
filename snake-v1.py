80f1bd9b3faaee5d86e3c5803370faa57ec69492
Louis Gallet <me@louisgallet.ga>
2022-02-12 10:19:59 +0100

    Initial commit

---
+++ snake-v1.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Programme Snake
+
+"""
+from tkinter import *
+from winreg import DisableReflectionKey # Importation de la bibliothèque  Tkinter 
+
+# On crée un environnement Tkinter
+tk = Tk()
+   
+# On crée un canevas dans l'environnement Tkinter d'une taille de 500x500
+# Ce constructeur prend comme premier paramètre l'objet dans lequel il sera
+# intégré (ici l'environnement Tkinter)
+# Les trois autres paramètres permettent de spécifier la taille et la couleur
+# de fond du canevas
+def computeNextFrame(numFrame, coordonnee):
+    """ print(numFrame) """
+    numFrame= numFrame + 1
+    can.delete('all')
+    if direction == 'left':
+        coordonnee[0] = coordonnee[0] - 20
+    elif direction == 'right':
+        coordonnee[0] = coordonnee[0] + 20
+    elif direction == 'up':
+        coordonnee[1] = coordonnee[1] - 20
+    elif direction == 'down':
+        coordonnee[1] = coordonnee[1] + 20
+    can.create_rectangle(coordonnee[0], coordonnee[1]+20, coordonnee[0]+20, coordonnee[1], fill='red', outline='yellow')
+    can.create_rectangle(coordonnee[0], coordonnee[1], coordonnee[0]+20, coordonnee[1]+20, fill='red', outline='yellow')
+    can.create_rectangle(coordonnee[0]+20, coordonnee[1], coordonnee[0]+40, coordonnee[1]+20, fill='red', outline='yellow')    
+    tk.after(100, lambda: computeNextFrame(numFrame, coordonnee))
+
+def right(event):
+    global direction
+    direction = 'right'
+    print(direction)
+def left(event):
+    global direction
+    direction = 'left'
+    print(direction)
+def up(event):
+    global direction
+    direction = 'up'
+    print(direction)
+def down(event):
+    global direction
+    direction = 'down'
+    print(direction)
+
+ 
+
+if __name__ == '__main__':
+    can = Canvas(tk, width=500, height=500, bg='black')
+    can.pack()
+    direction='up'
+    computeNextFrame(0, [400,400])
+    tk.bind('<Right>', right)
+    tk.bind('<Left>', left)
+    tk.bind('<Up>', up)
+    tk.bind('<Down>', down)
+    # On affiche le canevas
+    tk.mainloop() # Cet appel doit être la derniere instruction du programme
+
+
+
+
+""" can.create_rectangle(480, 0, 500,  20, fill='green', outline='yellow')
+can.create_oval(100, 200, 120, 120, fill='blue', outline='red') """
+
+""" computeNextFrame(0, coordonnee=400)
+ """
+
+# lancement de la boucle principale qui écoute les évènements (claviers...)
+
+
+
+
+
---
+++ test_snake.py
@@ -0,0 +1,171 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 09:54:11 2020
+
+@author: imac
+"""
+# -*- coding: utf-8 -*-
+"""
+Programme Snake
+
+"""
+
+from tkinter import *# Importation de la bibliothèque  Tkinter
+from tkinter import font as tkfont
+from random import randint
+
+from PIL import Image, ImageTk
+# On crée un environnement Tkinter
+tk = Tk()
+im_teteN = Image.open("teteN.png")
+teteN = ImageTk.PhotoImage(im_teteN)
+im_teteS = Image.open("teteS.png")
+teteS = ImageTk.PhotoImage(im_teteS)
+im_teteE = Image.open("teteE.png")
+teteE = ImageTk.PhotoImage(im_teteE)
+im_teteW = Image.open("teteW.png")
+teteW = ImageTk.PhotoImage(im_teteW)
+im_noeud1 = Image.open("noeud1.png")
+noeud1 = ImageTk.PhotoImage(im_noeud1)
+im_noeud2 = Image.open("noeud2.png")
+noeud2 = ImageTk.PhotoImage(im_noeud2)
+pomme = Image.open("pomme.png")
+pomme = ImageTk.PhotoImage(pomme)
+
+
+
+difficulte = int(input("Choisissez la difficulté du jeu :\n1.Facile\n2.Moyen\n3.Difficile\n"))
+
+
+
+def right(event):
+    # Modification de la variable globale direction
+    global direction
+    direction = 'right'
+    #print(direction)
+
+def left(event):
+    # Modification de la variable globale direction
+    global direction
+    direction = 'left'
+    #print(direction)
+
+def down(event):
+    # Modification de la variable globale direction
+    global direction
+    direction = 'down'
+    #print(direction)
+
+def up(event):
+    # Modification de la variable globale direction
+    global direction
+    direction = 'up'
+    #print(direction)
+
+def computeNextFrame(numFrame,coordonnee, objet):
+    global direction
+    # Affiche le numéro de la frame
+    #print(numFrame)
+    numFrame = numFrame + 1
+
+    can.delete("all")
+
+    for n in range (len(coordonnee)-1,0,-1):
+        coordonnee[n] = list(coordonnee[n-1])
+
+        # Mise à jour des coordonnées
+    if direction == 'right':
+        coordonnee[0][0] += 20
+        can.create_image(coordonnee[0][0], coordonnee[0][1], anchor = NW, image = teteE)
+        if coordonnee[0][0] > 500:
+            coordonnee[0][0] = 0
+    if direction == 'left':
+        coordonnee[0][0] -= 20
+        can.create_image(coordonnee[0][0], coordonnee[0][1], anchor = NW, image = teteW)
+        if coordonnee[0][0] < 0:
+            coordonnee[0][0] = 480
+    if direction == 'up':
+        coordonnee[0][1] -= 20
+        can.create_image(coordonnee[0][0], coordonnee[0][1], anchor = NW, image = teteN)
+        if coordonnee[0][1] < 0:
+            coordonnee[0][1] = 480
+    if direction == 'down':
+        coordonnee[0][1] += 20
+        can.create_image(coordonnee[0][0], coordonnee[0][1], anchor = NW, image = teteS)
+        if coordonnee[0][1] > 500:
+            coordonnee[0][1] = 0
+
+    for n in range(1,len(coordonnee)):
+        if n%2 == 0:
+            can.create_image(coordonnee[n][0], coordonnee[n][1], anchor = NW, image = noeud1)
+        else:
+            can.create_image(coordonnee[n][0], coordonnee[n][1], anchor = NW, image = noeud2)
+
+
+    can.create_image(objet[0], objet[1], anchor = NW, image = pomme)
+
+
+
+    if coordonnee[0][0] == objet [0] and coordonnee[0][1] == objet [1]:
+        # Déplacement de la pomme
+        objet[0] = randint(1,24)* 20
+        objet[1] = randint(1,24)* 20
+        # Ajout d'un noeud au serpent (à la même place que le dernier noeud)
+        coordonnee.append([-20, -20]) # Caché pour l'instant
+
+    game_over = False
+    # On test la position de la tête par rapport aux noeuds du serpent
+    for n in range(1,len(coordonnee)): # L'indice 0 est exclu, c'est la tête
+        if coordonnee[0][0] == coordonnee [n][0] and coordonnee[0][1] == coordonnee [n][1]:
+            game_over = True # La partie est finie
+
+    if game_over==True :
+        #Fin de partie
+        can.create_text(250,250,text = "GAME OVER", fill='red', font=("Helvetica",20,"bold"))
+    else:
+        # La partie n'est pas finie
+        #calcul une nouvelle frame dans 100 ms
+        if difficulte==1:
+            tk.after(300, lambda: computeNextFrame(numFrame,coordonnee, objet))
+        elif difficulte==2:
+            tk.after(100, lambda: computeNextFrame(numFrame,coordonnee, objet))
+        elif difficulte==3:
+            tk.after(50, lambda: computeNextFrame(numFrame,coordonnee, objet))
+
+if __name__=="__main__":
+    # On crée un environnement Tkinter
+    # On crée un canevas dans l'environnement Tkinter d'une taille de 500x500
+    # Ce constructeur prend comme premier paramètre l'objet dans lequel il sera
+    # intégré (ici l'environnement Tkinter)
+    # Les trois autres paramètres permettent de spécifier la taille et la couleur
+    # de fond du canevas
+    can = Canvas(tk, width=500, height=500, bg='black')
+    # On affiche le canevas
+    can.pack()
+    # Direction par défaut
+    direction = 'up'
+
+    coordonnee = [ [180, 200],
+                   [200, 200],
+                   [200, 240],
+                   [220, 240] ]
+
+    # Premier objet (la pomme)
+    x = randint(1,24)
+    y = randint(1,24)
+    x2 = randint(1,24)
+    y2 = randint(1,24)
+
+    objet = [x*20, y*20]
+
+    computeNextFrame(0,coordonnee, objet)
+
+    tk.bind('<d>', right)
+    tk.bind('<q>', left)
+    tk.bind('<s>', down)
+    tk.bind('<z>', up)
+    # lancement de la boucle principale qui écoute les évènements (claviers...)
+    tk.mainloop() # Cet appel doit être la derni ere instruction du programme
+
